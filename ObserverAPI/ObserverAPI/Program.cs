5925a415aca7246845f5a7e73ffaf501a9de87e9
Sahil Dafda <91382668+sahildafda@users.noreply.github.com>
2025-05-16 07:19:13 +0530

    Added CORS Policy
    To Resolve CROS Origin Error.

--- ObserverAPI/ObserverAPI/Program.cs
+++ ObserverAPI/ObserverAPI/Program.cs
@@ -2,10 +2,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers().AddNewtonsoftJson().AddJsonOptions(options =>
+builder.Services.AddCors(options =>
 {
-    options.JsonSerializerOptions.PropertyNamingPolicy = null; // Preserve PascalCase
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
 });
+
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver();
+    });
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -19,6 +31,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Use CORS here
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
